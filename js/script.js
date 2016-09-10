dbd712a07f8bacffc49a29d9aa407b12721a1b9d
Tayane Fernandes <tayane_fernandes@gap.com>
2016-09-10 11:22:35 -0300

    Refactoring code

--- js/script.js
+++ js/script.js
@@ -1,46 +1,52 @@
 function calcVacation() {
-  var initialDate = moment(document.getElementById('initialDate').value);
-  var numberOfDays = (document.getElementById('numberOfDays').value) - 1;
+  var initialDate = moment(document.getElementById('initialDate').value).startOf('day');
+  var qtdDays =  parseInt(document.getElementById('numberOfDays').value);
+  var numberOfDays = qtdDays - 1;
   var endDate = initialDate.clone().add(numberOfDays, 'day');
-  var qtdDias = endDate.diff(initialDate, 'days') + 1;
 
-  var count = 0
-  var curDate = initialDate.clone();
+  var daysOfWeek = countDaysOfWeek(initialDate, endDate);
+  var weekends = qtdDays - daysOfWeek;
+  qtdDays = calculateTotalDays(qtdDays);
 
-  while (curDate <= endDate) {
-      var dayOfWeek = curDate.day();
-      var isWeekend = (dayOfWeek == 6) || (dayOfWeek == 0);
-      if(!isWeekend) {
-         count++;
-      }
+  function calculateTotalDays(qtdDays) {
+    var totalDays = qtdDays;
+    if(initialDate.day() == 1) {
+      totalDays += 2;
+    } else if (initialDate.day() == 5){
+      totalDays += 1;
+    }
 
-      curDate = curDate.add(1, 'day');
-      if(curDate.isSame(endDate)) {
-          console.log('igual');
-      }
+    if(endDate.day() == 5) {
+      totalDays += 2;
+    } else if (endDate.day() == 6){
+      totalDays += 1;
+    }
+    return totalDays;
   }
 
-  var weekends = qtdDias - count;
-
-  console.log(initialDate.day());
-
-
-  console.log(endDate.weekday());
-  if(initialDate.day() == 1) {
-    qtdDias += 2;
-  } else if (initialDate.day() == 5){
-    qtdDias += 1;
+  function isWeekend(dayOfWeek) {
+    return (dayOfWeek == 6) || (dayOfWeek == 0);
   }
 
-  if(endDate.day() == 5) {
-    qtdDias += 2;
-  } else if (endDate.day() == 6){
-    qtdDias += 1;
+  function countDaysOfWeek(initialDate, endDate) {
+    var countDaysOfWeek = 0;
+    var curDate = initialDate.clone();
+
+    while (curDate <= endDate) {
+        var day = curDate.day();
+        if(!isWeekend(day)) {
+           countDaysOfWeek++;
+        }
+
+        curDate = curDate.add(1, 'day');
+    }
+
+    return countDaysOfWeek;
   }
 
 
   document.getElementById('result').innerHTML = '<p>Seu ultimo dias de ferias: ' + endDate.format('DD/MM/YYYY') + '</p>'
-   + '<p>Total Dias contando com finais de semana fora das ferias: ' + qtdDias + '</p>'
-   + '<p>Total Dias de Semana: ' + count + '</p>'
+   + '<p>Total Dias contando com finais de semana fora das ferias: ' + qtdDays + '</p>'
+   + '<p>Total Dias de Semana: ' + daysOfWeek + '</p>'
    + '<p>Total Finais de Semana perdidos nas ferias: ' + weekends + '</p>';
 }
