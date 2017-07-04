f61122ce40989d8e73118ab52486c2b22d026e37
Tayane Fernandes <tayane.rmf@gmail.com>
2017-07-04 16:59:01 -0300

    Add initial react component

---
+++ src/main/app.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import moment from 'moment'
+
+export default props => (
+  <div className='container'>
+    <h1>Descubra o melhor dia para tirar suas férias</h1>
+    <label for="initialDate">Data Inicial</label>
+    <input type="date" id="initialDate" />
+    <br/>
+    <label for="endDate">Quantos dias de ferias?</label>
+    <input type="number" id="numberOfDays" />
+    <br/>
+    <button onClick={calcVacation()}>Calcular</button>
+
+    <div id="result">
+
+    </div>
+  </div>
+)
+
+export const calcVacation = () => {
+  var initialDate = moment(document.getElementById('initialDate').value).startOf('day');
+  var qtdDays =  parseInt(document.getElementById('numberOfDays').value);
+  var numberOfDays = qtdDays - 1;
+  var endDate = initialDate.clone().add(numberOfDays, 'day');
+
+  var daysOfWeek = countDaysOfWeek(initialDate, endDate);
+  var weekends = qtdDays - daysOfWeek;
+  qtdDays = calculateTotalDays(qtdDays);
+
+  function calculateTotalDays(qtdDays) {
+    var totalDays = qtdDays;
+    if(initialDate.day() == 1) {
+      totalDays += 2;
+    } else if (initialDate.day() == 5){
+      totalDays += 1;
+    }
+
+    if(endDate.day() == 5) {
+      totalDays += 2;
+    } else if (endDate.day() == 6){
+      totalDays += 1;
+    }
+    return totalDays;
+  }
+
+  function isWeekend(dayOfWeek) {
+    return (dayOfWeek == 6) || (dayOfWeek == 0);
+  }
+
+  function countDaysOfWeek(initialDate, endDate) {
+    var countDaysOfWeek = 0;
+    var curDate = initialDate.clone();
+
+    while (curDate <= endDate) {
+        var day = curDate.day();
+        if(!isWeekend(day)) {
+           countDaysOfWeek++;
+        }
+
+        curDate = curDate.add(1, 'day');
+    }
+
+    return countDaysOfWeek;
+  }
+
+
+  document.getElementById('result').innerHTML = '<p>Seu ultimo dias de ferias: ' + endDate.format('DD/MM/YYYY') + '</p>'
+   + '<p>Total Dias contando com finais de semana fora das ferias: ' + qtdDays + '</p>'
+   + '<p>Total Dias de Semana: ' + daysOfWeek + '</p>'
+   + '<p>Total Finais de Semana perdidos nas ferias: ' + weekends + '</p>';
+}
