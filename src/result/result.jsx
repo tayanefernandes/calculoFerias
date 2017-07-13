22388016ee04b333c28210cba6e9fb1705ea6285
Tayane Fernandes <tayane.rmf@gmail.com>
2017-07-13 16:05:34 -0300

    Adicionando espaço para depois do total de dias

--- src/result/result.jsx
+++ src/result/result.jsx
@@ -12,7 +12,7 @@ const Result = (props) => {
     <div className='result'>
       <p>Seu último dias de férias: <strong>{props.endDateMoment}</strong> </p>
       <p>Total de dias contando com finais de semana fora das férias:
-         <strong>{props.totalDays}</strong>
+        <strong> {props.totalDays}</strong>
       </p>
       <p>Total de dias de semana: <strong>{props.daysOfWeek}</strong></p>
       <p>Total de dias não úteis perdidos nas férias: <strong>{props.daysOfWeekend}</strong></p>
